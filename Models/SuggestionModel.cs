0cc86c963be340a0d527d87f82fddd814d5f09cf
HopeMorehead <splendidwonder@gmail.com>
2017-07-25 10:36:07 -0400

    congrats

---
+++ Models/SuggestionModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuggestionBox.Models
+{
+    public class SuggestionModel
+    {
+
+        private string topic;
+        private string sugesstion;
+
+
+        [Key]
+        public int RecordNum { get; set; }
+
+
+        public string Topic
+        {
+            
+
+            get { return this.topic; }
+            set { this.topic = value; }
+        }
+
+        public string Suggestion
+        {
+
+            get{ return this.Suggestion; }
+            set { this.sugesstion = value; }
+
+
+        }
+
+
+
+    }
+}
